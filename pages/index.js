3c83e56db87683a40284b5ec88e26495f88e220f
comeprismic <come.sabran@prismic.io>
2022-12-14 13:47:56 +0100

    added seo fields to homepage

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,16 @@ import { PrismicRichText, SliceZone} from '@prismicio/react'
 import { createClient } from '../prismicio'
 import { components } from '../slices'
 import { Layout } from "../components/Layout";
+import { Head } from "next/head";
 
 
 const Page = ({ homepage, header, footer }) => {
   return (
     <section>
+    <Head>
+    <span >{ homepage.data.seo_title }</span>
+    <span >{ homepage.data.seo_description }</span>
+    </Head>
     <Layout nav={header} bottomnav={footer}>
     <PrismicRichText field={homepage.data.title} />
     <SliceZone slices={homepage.data.slices} components={components} />
